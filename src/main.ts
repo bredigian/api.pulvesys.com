d1fc07df11bc1f1a075a572a89256a5f7b4bb888
Gianluca Bredice <bredicegian@gmail.com>
2025-01-21 19:10:56 -0300

    config: enable cors and versioning and set config module for read enviroment variables

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.enableVersioning();
+
+  const PORT = process.env.PORT || 3000;
+
+  await app.listen(PORT, () =>
+    console.log('API Server is running at PORT', PORT),
+  );
 }
 bootstrap();
